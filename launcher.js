0d31c96233b94fe217e9475e161367552ebbb62c
Quentin <quentin.ceschin@gmail.com>
2019-12-02 15:00:05 +0100

    save

--- launcher.js
+++ launcher.js
@@ -2,10 +2,10 @@ const express = require('express');
 const serveStatic = require("serve-static");
 const path = require('path');
 var app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, '/dist/')));
 
-app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
+// app.get(/.*/, function(req, res) {
+//     res.sendfile(__dirname + "/dist/index.html");
+// });
 const port = process.env.PORT || 8080;
 app.listen(port);
