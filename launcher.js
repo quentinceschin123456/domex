02891bc42284296778d4c1210a7c9f42aa167105
Quentin <quentin.ceschin@gmail.com>
2019-12-02 19:56:18 +0100

    procfile

--- launcher.js
+++ launcher.js
@@ -1,11 +1,11 @@
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const express = require('express');
 const serveStatic = require("serve-static");
 const path = require('path');
 const app = express();
 
 // app.use(require('connect-history-api-fallback'));
-app.use(serveStatic(path.join(__dirname, '/dist/')));
+app.use(serveStatic(path.join(__dirname, '/dist')));
 
 // app.get(/.*/, function(req, res) {
 //     res.sendfile(__dirname + "/dist/index.html");
