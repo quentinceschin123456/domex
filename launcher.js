f73670bda7c3fda4db14f19de6ce9636533c0913
Quentin <quentin.ceschin@gmail.com>
2019-12-02 10:40:41 +0100

    fix

--- launcher.js
+++ launcher.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
+var serveStatic = require('serve-static');
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(serveStatic(__dirname + "/dist/"));
 
-app.get("/.*/", function(req, res) {
-    res.sendFile(__dirname + "/dist/index.html");
-});
 
 app.listen(port);
 
