f37986adebcb76e81d93b8b8d0ee7b1627fe0f56
Quentin <quentin.ceschin@gmail.com>
2019-12-02 19:04:53 +0100

    procfile

--- launcher.js
+++ launcher.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const express = require('express');
 const serveStatic = require("serve-static");
 const path = require('path');
