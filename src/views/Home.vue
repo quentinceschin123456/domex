029ff4bfb969b15f910ec3591b396a27a805f029
Quentin <quentin.ceschin@gmail.com>
2019-12-02 14:38:32 +0100

    config update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <a href="https://domexapplication.000webhostapp.com/test">test link</a>
+    <a href="http://localhost:5000/test">test link</a>
   </div>
 </template>
 
