7b7c36e276bf806ce9c3015be15ad4a89364c07d
Quentin <quentin.ceschin@gmail.com>
2019-12-02 10:54:50 +0100

    fix

--- launcher.js
+++ launcher.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
+var path = require('path');
 
 var port = process.env.PORT || 8080;
 
-app.use(serveStatic(__dirname + "/dist/"));
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
 
 app.listen(port);
