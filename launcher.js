34dc3549fbc573b986e5e97bfeca7592fd47b789
Quentin <quentin.ceschin@gmail.com>
2019-12-03 07:22:44 +0100

    update launcher

--- launcher.js
+++ launcher.js
@@ -1,13 +1,12 @@
+const express = require("express");
 const port = process.env.PORT || 8080;
-const express = require('express');
-const serveStatic = require("serve-static");
-const path = require('path');
 const app = express();
 
-// app.use(require('connect-history-api-fallback'));
-app.use(serveStatic(path.join(__dirname, '/dist')));
-
-// app.get(/.*/, function(req, res) {
-//     res.sendfile(__dirname + "/dist/index.html");
-// });
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
 app.listen(port);
+
+// eslint-disable-next-line no-console
+console.log("Server started...");
