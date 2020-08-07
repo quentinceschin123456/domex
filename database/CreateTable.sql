b20a0ded239016385e94d930bfc898b953381dc3
Quentin Ceschin <ceschin.quentin@gmail.com>
2020-08-08 01:24:03 +0200

    start db

--- database/CreateTable.sql
+++ database/CreateTable.sql
@@ -1,23 +1,23 @@
 Create table Groupe(
-
+id SERIAL PRIMARY KEY
 );
 
 Create table User(
-
+id SERIAL PRIMARY KEY
 );
 
 Create table Recette(
-
+id SERIAL PRIMARY KEY
 );
 
 Create table CourseRaw(
-
+id SERIAL PRIMARY KEY
 );
 
 Create table Recette(
-
+id SERIAL PRIMARY KEY
 );
 
 Create table Ingredient(
-
+id SERIAL PRIMARY KEY
 );
